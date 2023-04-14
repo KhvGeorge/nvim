1383194f0d82343b52c7b46902d421fefdc2c6f6
KhvGeorge <gxvedelidze@crocobet.com>
2023-04-14 01:58:00 -0700

    another update

--- lua/gamge/packer.lua
+++ lua/gamge/packer.lua
@@ -17,7 +17,13 @@ return require("packer").startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
-	use({ "rose-pine/neovim", as = "rose-pine" })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
 	vim.cmd("colorscheme rose-pine")
 
