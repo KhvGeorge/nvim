0219c1690d31abf6202bbec384fac9e51a5c38ae
KhvGeorge <gxvedelidze@crocobet.com>
2023-06-30 23:50:36 -0700

    up

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -132,7 +132,7 @@ require("nightfox").setup({
 })
 
 function ColorMyPencils(color)
-	color = color or "gruvbox-material"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
